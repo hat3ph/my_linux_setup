0ef6efd649c0e0223cdcf0c546ac93f9597237c7
hat3ph <88069788+hat3ph@users.noreply.github.com>
2024-09-03 12:44:41 +0800

    Update screen_brightness.sh

--- bin/screen_brightness.sh
+++ bin/screen_brightness.sh
@@ -5,13 +5,14 @@ notification_interval=3000
 brightness=$(brightnessctl info | cut -d ' ' -f 4 | grep -oP '\(\K[^%)]+')
 
 # use notify-send from libnotify-bin package or dunstify from dunst package
-if command -v notify-send >&2; then
+if command -v notify-send &> /dev/null; then
 	notification_cmd=/usr/bin/notify-send
 else
 	notification_cmd=/usr/bin/dunstify
 fi
 
 function send_notification() {
+	brightness=$(brightnessctl info | cut -d ' ' -f 4 | grep -oP '\(\K[^%)]+')
 	$notification_cmd -t $notification_interval -i audio-speakers -u low -r "9994" -h int:value:"$brightness" "Brightness Level $brightness%"
 }
 
@@ -19,14 +20,14 @@ case $1 in
 up)
 	# increase screen brightness level and send notification
 	brightnessctl set $brightness_level%+
-	send_notification $1
+	send_notification
 	;;
 down)
 	# decrease screen brightness level not more then 50% and send notification
 	if [ "$brightness" -gt 50 ]; then
 		brightnessctl set $brightness_level%-
 	fi
-	send_notification $1
+	send_notification
 	;;
 *)
 	echo "Usage: $0 {up|down}"
