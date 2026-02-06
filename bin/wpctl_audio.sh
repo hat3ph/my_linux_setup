80b732b3fc40eff4aa21847f9865cf8fd684c0b5
hat3ph <88069788+hat3ph@users.noreply.github.com>
2026-02-06 11:19:56 +0800

    add check to wpctl application

--- bin/wpctl_audio.sh
+++ bin/wpctl_audio.sh
@@ -2,6 +2,7 @@
 
 audio_level=5
 notification_interval=3000
+app=wpctl
 
 # use notify-send from libnotify-bin package or dunstify from dunst package
 if command -v notify-send >&2; then
@@ -10,8 +11,14 @@ else
 	notification_cmd=/usr/bin/dunstify
 fi
 
+# check wpctl application availability
+if ! command -v $app >&2; then
+	$notification_cmd -t $notification_interval -i audio-speakers -u critical "Missing $app application!!!"
+	exit 1
+fi
+
 function send_notification() {
-	volume=$(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 2 | awk '{printf "%2.0f%%\n", 100 * $1}')
+	volume=$($app get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 2 | awk '{printf "%2.0f%%\n", 100 * $1}')
 	$notification_cmd -t $notification_interval -i audio-speakers -u low -r "9993" -h int:value:"$volume" "Audio Level $volume"
 }
 
@@ -21,19 +28,19 @@ status)
 	;;
 up)
 	# increase audio level and send notification
-	wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ $audio_level%+
+	$app set-volume -l 1 @DEFAULT_AUDIO_SINK@ $audio_level%+
 	send_notification
 	;;
 down)
 	# decrease audio level and send notification
-	wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ $audio_level%-
+	$app set-volume -l 1 @DEFAULT_AUDIO_SINK@ $audio_level%-
 	send_notification
 	;;
 mute)
 	# toggle audio to mute or unmute
-	wpctl set-mute @DEFAULT_AUDIO_SINK@ toggle
+	$app set-mute @DEFAULT_AUDIO_SINK@ toggle
 	# send audio muted notification if muted or send audio level notification if not muted
-	if [[ -n $(wpctl get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 3 | sed 's/^.//;s/.$//') ]]; then
+	if [[ -n $($app get-volume @DEFAULT_AUDIO_SINK@ | cut -d ' ' -f 3 | sed 's/^.//;s/.$//') ]]; then
 		$notification_cmd -t $notification_interval -i audio-volume-muted "Audio Muted"
 	else
 		send_notification
@@ -41,9 +48,9 @@ mute)
 	;;
 mic)
 	# toggle microphone to mute or un-mute
-	wpctl set-mute @DEFAULT_AUDIO_SOURCE@ toggle
+	$app set-mute @DEFAULT_AUDIO_SOURCE@ toggle
 	# send notification if microphone is muted or un-muted
-	if [[ -n $(wpctl get-volume @DEFAULT_AUDIO_SOURCE@ | cut -d ' ' -f 3 | sed 's/^.//;s/.$//') ]]; then
+	if [[ -n $($app get-volume @DEFAULT_AUDIO_SOURCE@ | cut -d ' ' -f 3 | sed 's/^.//;s/.$//') ]]; then
 		$notification_cmd -t $notification_interval -i audio-input-microphone-muted "Microphone Muted"
 	else
 		$notification_cmd -t $notification_interval -i audio-input-microphone "Microphone Un-Muted"
