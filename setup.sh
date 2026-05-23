62be252fdc0d7f32dcaed64bbbec2ed91e625d3d
hat3ph <88069788+hat3ph@users.noreply.github.com>
2026-05-23 10:11:47 +0800

    remove lxapperance for xfwm4

--- setup.sh
+++ setup.sh
@@ -283,7 +283,7 @@ function install(){
 			# install x server
 			install_xserver
 			# install xfwm4 and other packages
-			install_packages xinit $terminal xfwm4 xfce4-panel sxhkd feh lxappearance dex flameshot rofi
+			install_packages xinit $terminal xfwm4 xfce4-panel sxhkd feh dex flameshot rofi
 			echo "exec xfwm4" > $HOME/.xinitrc
 			cp ./xfwm4/xsessionrc $HOME/.xsessionrc
 			
